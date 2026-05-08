408e73c61311f5be7fec3b47a35fd6a7902104b3
MdAdilRaza-an <adilrazamd12@gmail.com>
2026-05-08 22:45:14 +0530

    mysql ssl added

--- MyAuthBackend/config/db.js
+++ MyAuthBackend/config/db.js
@@ -6,13 +6,18 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+
+    ssl:{
+        rejectUnauthorized:false
+    }
 });
 
 db.connect((err) => {
 
     if (err) {
-        console.log(err);
+        console.log("DB Error ❌", err);
     } else {
         console.log("MySQL Connected ✅");
     }
